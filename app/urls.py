9b2f7794d4049fff2b6dd2c99bffb0ecf05f8649
Ben Whalley <benwhalley@gmail.con>
2015-03-03 01:07:17 +0000

    .

--- app/urls.py
+++ app/urls.py
@@ -5,10 +5,10 @@ from tools.views import *
 
 urlpatterns = patterns('',
 
-	url(r'^', ListView.as_view(model=Problem) , name="home"),
+
     url(r'^problems/', ListView.as_view(model=Problem) , name="problems"),
     url(r'^problem/(?P<slug>[-\w]+)/', DetailView.as_view(model=Problem) , name="problem"),
     url(r'^worksheet/(?P<slug>[-\w]+)/', DetailView.as_view(model=Worksheet) , name="worksheet"),
-
+	url(r'^', ListView.as_view(model=Problem) , name="home"),
     url(r'^admin/', include(admin.site.urls)),
 )
