2138113f71408fd3cfe3ed30890adf0a72783479
Ben Whalley <benwhalley@gmail.con>
2015-03-03 01:25:10 +0000

    .

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import ListView, DetailView
-from tools.views import *
+from tools.models import *
 
 urlpatterns = patterns('',
 
     
     url(r'^problem/(?P<slug>[-\w]+)/', DetailView.as_view(model=Problem) , name="problem"),
     url(r'^worksheet/(?P<slug>[-\w]+)/', DetailView.as_view(model=Worksheet) , name="worksheet"),
+    url(r'^', ListView.as_view(model=Problem), name="problems"),
+
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', ListView.as_view(model=Problem) , name="problems"),
 )
