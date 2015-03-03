221d7c0b0edcc949916b2e9f18fbd091a944e7e4
Ben Whalley <benwhalley@gmail.con>
2015-03-03 01:04:14 +0000

    .

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,8 @@ from django.views.generic import ListView, DetailView
 from tools.views import *
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'psytools.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+	url(r'^', ListView.as_view(model=Problem) , name="home"),
     url(r'^problems/', ListView.as_view(model=Problem) , name="problems"),
     url(r'^problem/(?P<slug>[-\w]+)/', DetailView.as_view(model=Problem) , name="problem"),
     url(r'^worksheet/(?P<slug>[-\w]+)/', DetailView.as_view(model=Worksheet) , name="worksheet"),
